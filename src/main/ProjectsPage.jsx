efb1b61da9756b8a019028d7607ff7079fb399d2
Richard <richardliuwa@gmail.com>
2019-01-06 19:57:21 -0800

    added two new projects

--- src/main/ProjectsPage.jsx
+++ src/main/ProjectsPage.jsx
@@ -3,7 +3,9 @@ import Project from './Project';
 import camp_us from './camp-us-thumbnail.png';
 import treecursion from './p5-thumbnail.png';
 import attendeaux from './arduino-thumbnail.png';
+import portal from './teacher-portal.png';
 import e3scooter from './e3scooter-thumbnail.png';
+import where2meet from './where2meet.png';
 
 export default class ProjectsPage extends Component {
     render() {
@@ -11,6 +13,22 @@ export default class ProjectsPage extends Component {
             <div className="projects-container container">
                 <div className="jumbotron" id="header" data-aos="fade-up" data-aos-duration="2000">
                     <h1>P R O J E C T S</h1>
+                </div>
+                <div data-aos="zoom-in-up" data-aos-duration="3000" data-aos-delay="500">
+                    <Project name="YVHS Teacher Portal"
+                            technologies="Amazon S3 | React.js | Express.js | PostreSQL"
+                            description="Comprehensive web portal for teachers at Ygnacio Valley High School to organize their lessons and collect student feedback on their courses. Planned deployment date early 2019."
+                            img_url={portal}
+                            link=""
+                    />
+                </div>
+                <div data-aos="zoom-in-up" data-aos-duration="3000" data-aos-delay="500">
+                    <Project name="Where2Meet"
+                            technologies="Firebase | Google Maps API | Yelp API | React.js"
+                            description="Web app that finds the best place for a group to meet by finding venues on Yelp near the midpoint of their locations."
+                            img_url={where2meet}
+                            link="https://wheretwomeet.firebaseapp.com/"
+                    />
                 </div>
                 <div data-aos="zoom-in-up" data-aos-duration="3000" data-aos-delay="500">
                     <Project name="Attendeaux"

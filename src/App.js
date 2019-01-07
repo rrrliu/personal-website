e70cd106786fce24e5b8504dcb1b22ed09cc2826
Richard <richardliuwa@gmail.com>
2019-01-06 17:06:27 -0800

    changed homepage, removed browserrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './main/Home';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
